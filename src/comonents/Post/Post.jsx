041716f7d62068811fe56565585d1132c6f1b4e1
korathbasil <korathbasil@live.com>
2020-10-04 18:58:43 +0530

    removed firebase from frontend, will go with cloud fns later

--- src/comonents/Post/Post.jsx
+++ src/comonents/Post/Post.jsx
@@ -1,8 +1,52 @@
 import React from "react";
 import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
+import BookmarkBorderOutlinedIcon from "@material-ui/icons/BookmarkBorderOutlined";
+import { IconButton } from "@material-ui/core";
 
-function Post() {
-  return <div>Iam Post</div>;
+function Post({ avatar, userName, postImage, caption }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <div className="post__headerInfo">
+          <Avatar src={avatar} />
+          <h5 className="post__headerInfoUserName">{userName}</h5>
+        </div>
+        <MoreHorizIcon className="post__headerInfoAvatar" />
+      </div>
+      <div className="post__imageContainer">
+        <img className="post__headerImage" src={postImage} alt="" />
+      </div>
+      <div className="post__footer">
+        <div className="post__footerOptions">
+          <div className="post__footerOptionsReactions">
+            <IconButton>
+              <FavoriteBorderIcon />
+            </IconButton>
+            <IconButton>
+              <ChatBubbleOutlineOutlinedIcon />
+            </IconButton>
+            <IconButton>
+              <SendOutlinedIcon />
+            </IconButton>
+          </div>
+          <IconButton>
+            <BookmarkBorderOutlinedIcon />
+          </IconButton>
+        </div>
+        <div className="post__footerLikeReport">
+          <p>Johgn Lucas and 3 others like this image</p>
+        </div>
+        <p>
+          <strong>{userName}</strong> {caption}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
