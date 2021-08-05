c7b6c1dd1656e485a9cb7f911737503ebc758b45
Bazil Korath <korathbasil@live.com>
2021-08-05 20:47:33 +0530

    add login page

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import styles from ".login.module.css";
+
+const Login: React.FC = () => {
+  return (
+    <div className={styles.parent}>
+      <p>Login page</p>
+    </div>
+  );
+};
+
+export default Login;
