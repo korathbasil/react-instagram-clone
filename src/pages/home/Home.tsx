730f3e4558396806cb99faf42e60845e709bcfd0
Bazil Korath <korathbasil@live.com>
2021-08-05 20:45:11 +0530

    updates to react 17

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from "./home.module.css";
+
+const Home: React.FC = () => {
+  return (
+    <div className={styles.parent}>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
