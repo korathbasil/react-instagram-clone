d3855a3e7e8b2bacae1af83c2196985b8bf68550
Bazil Korath <korathbasil@live.com>
2021-08-09 22:49:25 +0530

    fix: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
+
 import "./App.css";
 import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
+// import Home from "./components/Home/Home";
+// import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 
 function App() {
   const [posts, setPosts] = useState([]);
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import styles from ".login.module.css";
+import styles from "./login.module.css";
 
 const Login: React.FC = () => {
   return (
