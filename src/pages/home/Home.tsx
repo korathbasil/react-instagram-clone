cd3eae3b028c53cde69cf7198896801cb2103fe8
Bazil Korath <korathbasil@live.com>
2021-08-22 21:20:29 +0530

    add post comp into home for testing purposes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,19 @@
 import Header from "../../components/header/Header";
 import styles from "./home.module.scss";
+import Post from "components/post";
 
 const Home: React.FC = () => {
   return (
     <div className={styles.parent}>
       <Header />
+      <div className={styles.container}>
+        <Post
+          avatar="https://upload.wikimedia.org/wikipedia/commons/d/df/Sabrina_Carpenter_2019.jpg"
+          userName="John Lucas"
+          postImage="https://upload.wikimedia.org/wikipedia/commons/d/df/Sabrina_Carpenter_2019.jpg"
+          caption="Hello World"
+        />
+      </div>
     </div>
   );
 };
