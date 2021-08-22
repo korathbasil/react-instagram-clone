e6f15ea39c30487c66a096ffaf111809e2242b50
Bazil Korath <korathbasil@live.com>
2021-08-22 21:40:25 +0530

    fix: change postContainer width

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,8 +6,11 @@ const Home: React.FC = () => {
   return (
     <div className={styles.parent}>
       <Header />
-      <div className={styles.container}>
-        <PostContainer />
+      <div className={styles.main}>
+        <div className={styles.left}>
+          <PostContainer />
+        </div>
+        <div className={styles.right}></div>
       </div>
     </div>
   );
