462d280e5fd9abf2555571977bfdbfb9f87b4ee7
Bazil Korath <korathbasil@live.com>
2021-08-30 19:25:41 +0530

    add styles to profile menu

--- src/components/profileDropdownMenu/ProfileDropdownMenu.tsx
+++ src/components/profileDropdownMenu/ProfileDropdownMenu.tsx
@@ -1,5 +1,12 @@
 import styles from "./profileDropdownMenu.module.scss";
 
+import {
+  ProfileLined,
+  SaveLined,
+  SettingsLined,
+  SwitchAccountLined,
+} from "assets/icons";
+
 interface IProfileDropdownMenuProps {
   openedMenu: string;
   setOpenedMenu: React.Dispatch<React.SetStateAction<string>>;
@@ -11,7 +18,28 @@ const ProfileDropdownMenu: React.FC<IProfileDropdownMenuProps> = ({
   return (
     <div>
       {openedMenu === "profile" && (
-        <div className={styles.menu}>Notifications</div>
+        <div className={styles.menu}>
+          <div className={styles.option}>
+            <ProfileLined />
+            <p>Profile</p>
+          </div>
+          <div className={styles.option}>
+            <SaveLined />
+            <p>Saved</p>
+          </div>
+          <div className={styles.option}>
+            <SettingsLined />
+            <p>Settings</p>
+          </div>
+          <div className={styles.option}>
+            <SwitchAccountLined />
+            <p>Switch Accounts</p>
+          </div>
+          <hr color="lightgrey" />
+          <div className={styles.logout}>
+            <p>Logout</p>
+          </div>
+        </div>
       )}
     </div>
   );
