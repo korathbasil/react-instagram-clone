512c5ca8ac375c5c860536ed01b6c39be185082a
Bazil Korath <korathbasil@live.com>
2021-09-06 23:51:38 +0530

    add basic redux setup with redux-toolkit

---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {},
+  reducers: {
+    setUser: () => {},
+  },
+});
+
+export default userSlice.reducer;
