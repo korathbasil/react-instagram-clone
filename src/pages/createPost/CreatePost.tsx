7a28166cfadd24236d6409721fbff39128d29949
Bazil Korath <korathbasil@live.com>
2021-09-09 18:49:41 +0530

    minor changes

--- src/pages/createPost/CreatePost.tsx
+++ src/pages/createPost/CreatePost.tsx
@@ -1,5 +1,13 @@
+import styles from "./createPost.module.scss";
+import Header from "components/header";
+
 const CretePost = () => {
-  return <div>Post</div>;
+  return (
+    <div className={styles.parent}>
+      <Header activeRoute="" />
+      <div className={styles.contents}></div>
+    </div>
+  );
 };
 
 export default CretePost;
