6d649077bf48b2f91b27338a71182c715910fe13
Bazil Korath <korathbasil@live.com>
2021-12-09 03:12:12 +0530

    add signup form

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,5 +1,58 @@
+import styles from "./signup.module.scss";
+
+import Logo from "assets/images/instagram-logo-black.png";
+
 const Signup = () => {
-    return <div>Signup page</div>
-}
+  return (
+    <div className={styles.parent}>
+      <div className={styles.child}>
+        <form action="" className={styles.signupForm}>
+          <img src={Logo} alt="" />
+          <h6>
+            <span>Sign up to see photos and</span>
+            <span>videos from your friends.</span>
+          </h6>
+
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            placeholder="Mobile number or email address"
+            required
+            onChange={loginInputValueModifier}
+          />
+
+          <input
+            type="text"
+            name="name"
+            data-testid="name-input"
+            placeholder="Full Name"
+            required
+            onChange={loginInputValueModifier}
+          />
+
+          <input
+            type="username"
+            name="username"
+            data-testid="username-input"
+            placeholder="Uername"
+            required
+            onChange={loginInputValueModifier}
+          />
+
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Password"
+            required
+            onChange={loginInputValueModifier}
+          />
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
