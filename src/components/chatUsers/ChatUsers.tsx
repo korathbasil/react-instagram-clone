16f71086bd42cbf32674fcc53ef36b6df8cc8b88
Bazil Korath <korathbasil@live.com>
2021-12-24 02:00:06 +0530

    add user seletor

--- src/components/chatUsers/ChatUsers.tsx
+++ src/components/chatUsers/ChatUsers.tsx
@@ -12,7 +12,44 @@ export const ChatUsers = () => {
         </div>
       </div>
 
-      <div className={styles.people}></div>
+      <div className={styles.people}>
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+        <UserSelector />
+      </div>
     </section>
+  );
+};
+
+const UserSelector = () => {
+  return (
+    <div className={styles.userSelector}>
+      <img
+        src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt=""
+      />
+      <div className={styles.userLabel}>
+        <p>User</p>
+        <p>last message was </p>
+      </div>
+      <div className={styles.unread}></div>
+    </div>
   );
 };
